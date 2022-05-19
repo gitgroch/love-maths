8fc744611badb4d28fb38baa506f75657b880975
gitgroch <geoff.rochford@gmail.com>
2022-05-19 11:37:46 +0000

    add code for substraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,15 +28,12 @@ function runGame(gameType) {
        displayAdditionQuestion(num1, num2);
    } else if (gameType === "multiply") {
        displayMultiplyQuestion(num1, num2);
+   } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
    } else {
        alert(`Unknown game type: ${gameType}` );
        throw `Unknown game type: ${gameType}. Aborting!`;
    }
-
-
-
-
- 
 }
 
 /**
@@ -73,7 +70,10 @@ function calculateCorrectAnswer() {
     } else if 
         (operator === "x") {
             return [operand1 * operand2, "multiply"];
-    } else {
+    } else if 
+    (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}. Aborting! `
     }
@@ -104,8 +104,12 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
-  
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
+
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
